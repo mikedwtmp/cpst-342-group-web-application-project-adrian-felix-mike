e0c2666fd3bc14d4d0fff61cafc0936df102fc82
mikedwtmp <121983831+mikedwtmp@users.noreply.github.com>
2023-11-08 20:27:00 +0000

    fixed typo inindex.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("./home",  function(req,res) {
+app.get("/home",  function(req,res) {
     res.send("<h1> This is the home page.</>");
 });
 
