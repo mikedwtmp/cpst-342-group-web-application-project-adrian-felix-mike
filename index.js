c261a4c5aa52981879aa2357a1353b2665743d7a
mikedwtmp <121983831+mikedwtmp@users.noreply.github.com>
2023-11-12 14:34:24 +0000

    added an additional funciton to index.js

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const express = require("express");
 const app = express();
 
 app.get("/home",  function(req,res) {
-    res.send("<h1> This is the home page.</>");
+    res.send("<h1> This is the home page.</h>");
+});
+
+app.get("/user/:user", function (req, res) {
+    user = req.params.user;
+    res.send("<h>This would be the page for user: " + user + "</h>" )
 });
 
 app.listen(3000);
