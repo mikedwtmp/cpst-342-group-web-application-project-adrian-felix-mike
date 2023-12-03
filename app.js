1f7768b65b8f3ff111381e3d066737038b9c1ea3
Michael Dreis <mdreis@luc.edu>
2023-12-02 18:51:28 -0600

    CRUD functionality built out fully

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const express = require('express')
+const express = require('express');
 const dbOperations = require('./database.js');
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 app.use(express.static('assets'))
 app.set("view engine", "hbs");
@@ -10,6 +10,42 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.render('home.hbs');
+})
+
+app.post('/authenticate', function(req, res) {
+    console.log(req.body)
+    console.log(`authenticating ${req.body.userName}...`)
+    dbOperations.lookUpUser(req.body.userName, res);
+})
+
+app.post('/dashboard', (req, res) => {
+     //continue here
+     let userID = req.body.userID;
+     dbOperations.getAllAndRender(userID, res);
+})
+
+app.post('/addNewMed', (req, res) => {
+    var userID = req.body.userID;
+    res.render('newMedForm.hbs', {userID: userID});
+})
+
+app.post('/createMedEntry', (req, res) => {
+    dbOperations.addNewMedication(req.body, res);
+})
+
+app.post('/deleteMed', (req, res) => {
+    console.log(req.body);
+    dbOperations.deleteMed(req.body.medID, req.body.userID, res);
+})
+
+app.post('/updateMed', (req, res) => {
+    console.log(req.body);
+    dbOperations.lookUpMedToUpdate(req.body.medID, req.body.userID, res);
+})
+
+app.post('/updateMedEntry', (req, res) => {
+    console.log(req.body)
+    dbOperations.updateAndRender(req.body, res);
 })
 
 app.listen(port)
--- database.js
+++ database.js
@@ -9,6 +9,99 @@ let db = new sqlite3.Database('./med_record_db.db', sqlite3.OPEN_READWRITE, (err
 	}
 });
 
+let lookUpUser = (userName, res) => {
+    var lookUpUser = "SELECT * from userInfo WHERE userName = ?";
+    db.each(lookUpUser, [userName], (err, row) => {
+        if (err) {
+            console.error(err.message)
+            throw err
+        }
+        if(!row) {
+            res.render('/usernotfound');
+        }else{
+            console.log(row)
+            res.render('loginSuccess.hbs', row);
+        }
+
+    })
+
+}
+
+let getAllAndRender = (userID, res) => {
+    var medTableName = "medTableID" + userID;
+    var query = "SELECT * from " + medTableName;
+    db.all(query, (err, rows) => {
+        if (err) {
+            console.error(err.message)
+            throw err
+        }
+        
+        var params = {userID: userID, rows: rows};
+        console.log(params)
+        res.render('dashboard.hbs', params);
+    
+    })
+
+}
+
+let deleteMed = (medID, userID, res) => {
+    var deleteQuery = "DELETE from medTableID" + userID + " WHERE medID = ?";
+    db.run(deleteQuery, [medID], (err) => {
+        if (err) {
+            console.error(err.message)
+            throw err
+        }
+        getAllAndRender(userID, res);
+    })
+}
+
+let addNewMedication = (medInfoObject, res) => {
+    console.log(medInfoObject);
+    //var userID = number(medInfoObject.userID)
+    var addMedQuery = "INSERT INTO medTableID" + medInfoObject.userID + " (medName, medDose, doseUnit, frequency, comment, userID) VALUES (?, ?, ?, ?, ?, ?)";
+    var params = [medInfoObject.medName, medInfoObject.medDose, medInfoObject.doseUnit, medInfoObject.frequency, medInfoObject.comment, medInfoObject.userID];
+    console.log(params);
+    db.run(addMedQuery, params, (err) => {
+        if (err) {
+            console.error(err.message)
+            throw err
+        }
+        console.log(res)
+        getAllAndRender(medInfoObject.userID, res);
+        
+        
+    })
+    
+
+}
+
+let lookUpMedToUpdate = (medID, userID, res) => {
+    
+    queryForUpdate = "SELECT * from medTableID" + userID + " WHERE medID = ?";
+    db.get(queryForUpdate, medID, (err, row) => {
+        if (err) {
+            console.error(err.message)
+            throw err
+        }
+        console.log(row);
+        res.render('updateForm.hbs', row);
+
+    })
+}
+
+let updateAndRender = (updateParams, res) => {
+    var updateQuery = "UPDATE medTableID" + updateParams.userID + " SET medName = ?, medDose = ?, doseUnit = ?, frequency = ?, comment = ? WHERE medID = ?";
+    params = [updateParams.medName, updateParams.medDose, updateParams.doseUnit, updateParams.frequency, updateParams.comment, updateParams.medID];
+    db.run(updateQuery, params, (err) => {
+        if (err) {
+            console.error(err.message)
+            throw err
+        }
+        getAllAndRender(updateParams.userID, res);
+    })
+}
+
+module.exports = {lookUpUser, getAllAndRender, deleteMed, addNewMedication, lookUpMedToUpdate, updateAndRender};
 
 // var addUser = "INSERT INTO userInfo (userName, fName, lName, age, streetAddress, ZIP) VALUES (?, ?, ?, ?, ?, ?)";
 // var params = ['lWolf', 'Lu', 'Wolf', 21, '1032 W Sheridan Rd', 60660];
