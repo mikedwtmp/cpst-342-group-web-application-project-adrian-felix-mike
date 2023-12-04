aab8b29c4fb19fb990ad5c22b34b40d138fd4ff3
Felix Oke <bfiliks4xt@gmail.com>
2023-12-04 13:49:19 -0600

    Update app.js
    Adding comments to each route

--- app.js
+++ app.js
@@ -13,32 +13,38 @@ app.get('/', function (req, res) {
     res.render('home.hbs');
 })
 
+// Route to authenticate User
 app.post('/authenticate', function(req, res) {
     console.log(req.body)
     console.log(`authenticating ${req.body.userName}...`)
     dbOperations.lookUpUser(req.body.userName, res);
 })
 
+// Route to authenticate User
 app.post('/dashboard', (req, res) => {
      //continue here
      let userID = req.body.userID;
      dbOperations.getAllAndRender(userID, res);
 })
 
+// Route to create New Medical Detail
 app.post('/addNewMed', (req, res) => {
     var userID = req.body.userID;
     res.render('newMedForm.hbs', {userID: userID});
 })
 
+// Route to create New Medical Detail
 app.post('/createMedEntry', (req, res) => {
     dbOperations.addNewMedication(req.body, res);
 })
 
+// Route to delete Medical Detail
 app.post('/deleteMed', (req, res) => {
     console.log(req.body);
     dbOperations.deleteMed(req.body.medID, req.body.userID, res);
 })
 
+// Route to update Medical Detail
 app.post('/updateMed', (req, res) => {
     console.log(req.body);
     dbOperations.lookUpMedToUpdate(req.body.medID, req.body.userID, res);
@@ -49,10 +55,12 @@ app.post('/updateMedEntry', (req, res) => {
     dbOperations.updateAndRender(req.body, res);
 })
 
+// Route to show Covid alers
 app.post('/covidAlert', (req, res) => {
     console.log("covid app" + req.body.userID);
     dbOperations.getCovidData(req.body.userID, res)
 })
+
 
 app.post('/usernotfound', (req, res) => {
     res.render('usernotfound.hbs');
