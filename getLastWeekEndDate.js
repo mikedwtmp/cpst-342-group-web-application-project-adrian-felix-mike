965af81b3d0a5798d7c9d2253b16479e74f53101
mikedwtmp <121983831+mikedwtmp@users.noreply.github.com>
2023-12-14 14:14:19 -0600

    Update getLastWeekEndDate.js

--- getLastWeekEndDate.js
+++ getLastWeekEndDate.js
@@ -4,7 +4,7 @@ let getLastWeekEndDate = () => {
     today.setHours(-(today.getTimezoneOffset() / 60),0,0,0)
     //this takes the current date and moves back to saturday, which is the final day of the week for the API
     today.setDate(today.getDate() - today.getDay() - 9);
-    console.log(today.getDate() + today.getDay());
+    console.log(today.getDay());
     return today;
 }
 
